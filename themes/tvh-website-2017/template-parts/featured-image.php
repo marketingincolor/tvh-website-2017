ccdc0d79672ab6dd83a20930305eb405657a8d20
eddt <etwilbeck@marketingincolor.com>
2017-09-18 15:00:52 -0500

    new alt mobile images enabled

--- themes/tvh-website-2017/template-parts/featured-image.php
+++ themes/tvh-website-2017/template-parts/featured-image.php
@@ -1,18 +1,33 @@
 <?php
 // If a featured image is set, insert into layout and use Interchange
 // to select the optimal image size per named media query.
-if ( has_post_thumbnail( $post->ID ) ) : ?>
-	<header class="featured-hero row expanded" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
+$small_thumb = get_field('special_thumb') ? get_field('special_thumb') : "" ;
+if ( (has_post_thumbnail( $post->ID )) && $small_thumb == "" ) : ?>
+	<header class="featured-hero row expanded no-thumb" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
 	<?php if ( $caption = get_post( get_post_thumbnail_id() )->post_excerpt ) : ?>
     <div class="featured-display medium-10 medium-centered">
     	<div class="featured-caption"><?php echo $caption; ?></div>
     </div>
 	<?php endif; ?>
-
 	</header>
 
 <?php else : ?>
-	<?php /* <header class="featured-hero row expanded" role="banner" data-interchange="[//satyr.io/1440x365/seagreen.png, small], [//satyr.io/1440x365/seagreen.png, medium], [//satyr.io/1440x365/seagreen.png, large], [//satyr.io/1440x365/seagreen.png, xlarge]">
-	</header> */ ?>
+
+	<header class="featured-hero row expanded show-for-medium" role="banner" data-interchange="[<?php echo $small_thumb['url']; ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
+	<?php if ( $caption = get_post( get_post_thumbnail_id() )->post_excerpt ) : ?>
+    <div class="featured-display medium-10 medium-centered">
+    	<div class="featured-caption"><?php echo $caption; ?></div>
+    </div>
+	<?php endif; ?>
+	</header>
+
+	<header class="sm-featured-hero row expanded show-for-small-only" role="banner" data-interchange="[<?php echo $small_thumb['url']; ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium]">
+	<?php if ( $caption = get_post( get_post_thumbnail_id() )->post_excerpt ) : ?>
+    <div class="featured-display medium-10 medium-centered">
+    	<div class="featured-caption"><?php echo $caption; ?></div>
+    </div>
+	<?php endif; ?>
+	</header>	
+
 <?php endif; ?>
 
