60716f679e2092bff0b7680000e923e3e71afa56
eddt <etwilbeck@marketingincolor.com>
2017-11-08 16:35:22 -0600

    new map changes

--- themes/tvh-website-2017/sidebar-locations.php
+++ themes/tvh-website-2017/sidebar-locations.php
@@ -14,6 +14,7 @@
 	$location_country = get_post_meta($location_id, '_location_country', true);
 	$location_phone = get_post_meta($location_id, 'location_phone', true);
 	$location_time = get_post_meta($location_id, 'location_time', true);
+	$location_gmap = get_post_meta($location_id, 'location_gmap', true);
 	$location_slug = get_post_field('post_name', $location_id);
 ?>
 <aside class="sidebar sidebar-location">
@@ -42,6 +43,11 @@
 		<article id="widget-3" class="widget widget-map">
 			<?php echo do_shortcode($location_code); ?>
 		</article>
+	<?php if ( $location_gmap != '') : ?>
+		<article style="background-color:#fff; text-align:center;">
+			<a href="<?php echo $location_gmap; ?>" target="_blank">See this location on Google Maps</a>
+		</article>
+	<?php endif; ?>
 	<?php if ( $location_slug != 'acute-care-clinic' ) : ?>
 		<article id="widget-4" class="widget widget-insurance">
 			<?php get_template_part( 'template-parts/accepted-insurance' ); ?>
